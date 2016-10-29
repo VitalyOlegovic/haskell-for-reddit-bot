87a59337b8fb3c22a55efc019a94eb776f65895c
vitalijzad <vitalij_zad@libero.it>
2016-10-29 18:51:44 +0200

    Fixed! It works!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Lib
     ( someFunc, hello
@@ -7,27 +8,43 @@ module Lib
 
 import Database.MySQL.Simple
 import Database.MySQL.Simple.QueryResults
+import Database.MySQL.Simple.Result
 
 connectInfo :: ConnectInfo
 connectInfo = ConnectInfo "192.168.1.7" 3306 "root" "root" "reddit_bot" [] "" Nothing
 
---This type reflects the database table structure
-data Subreddit = Subreddit{id :: Int, name :: String, daily_quota :: Int, priority :: Int, enabled :: Bool, recent_feeds_window :: Int, moderator :: Bool}
 
--- I have to fix this:
+data Subreddit = Subreddit{
+    id :: Integer
+  , name :: String
+  , daily_quota :: Integer
+  , priority :: Integer
+  , enabled :: Bool
+  , recent_feeds_window :: Maybe Integer
+  , moderator :: Bool
+  } deriving Show
+
 instance QueryResults Subreddit where
-    convertResults [fa,fb] [va,vb] = User $ a * b
-        where !a = convert fa va --Illegal bang-pattern (use BangPatterns)
-              !b = convert fb vb --Illegal bang-pattern (use BangPatterns)
+    convertResults [f1,f2,f3,f4,f5,f6,f7] [v1,v2,v3,v4,v5,v6,v7] 
+      = Subreddit x1 x2 x3 x4 x5 x6 x7
+     where 
+       !x1 = convert f1 v1 
+       !x2 = convert f2 v2
+       !x3 = convert f3 v3
+       !x4 = convert f4 v4
+       !x5 = convert f5 v5
+       !x6 = convert f6 v6
+       !x7 = convert f7 v7
+              
     convertResults fs vs  = convertError fs vs 2
-
-
-
-hello :: IO Subreddit
+    
+hello :: IO [Subreddit]
 hello = do
   conn <- connect connectInfo
-  [Only i] <- query_ conn "select * from subreddits" 
-  return i
+  r <- query_ conn "select * from subreddits" 
+  Prelude.putStrLn $ show r
+  return r
+
 
 someFunc :: IO ()
 someFunc = Prelude.putStrLn "someFunc"
